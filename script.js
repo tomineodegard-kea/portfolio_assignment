c50aa9c6c027782983d046d8d4c2ff9c83a257a7
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-03-24 23:05:53 +0100

    a whole bunch of styling and changes..

--- script.js
+++ script.js
@@ -20,3 +20,44 @@ function toggleMenu() {
     document.querySelector("#menu_btn").textContent = "✕";
   }
 }
+
+// // ----------- GSAP SCROLL TRIGGER
+// // keep at bottom
+// ScrollTrigger.create({
+//   trigger: ".sticky-today",
+//   start: "top bottom-=150",
+//   endTrigger: ".today-content",
+//   end: "top bottom-=75",
+//   pin: true,
+//   pinSpacing: false,
+// });
+
+// // keep x on top of the page
+// ScrollTrigger.create({
+//   trigger: ".sticky-today",
+//   start: "top top+=75",
+//   endTrigger: "html",
+//   end: "top bottom",
+//   pin: true,
+//   pinSpacing: false,
+// });
+
+// // keep the next thing at bottom
+// ScrollTrigger.create({
+//   trigger: ".sticky-tomorrow",
+//   start: "top bottom-=75",
+//   endTrigger: ".tomorrow-content",
+//   end: "bottom bottom-=75",
+//   pin: true,
+//   pinSpacing: false,
+// });
+
+// // keep at top
+// ScrollTrigger.create({
+//   trigger: ".sticky-tomorrow",
+//   start: "top top+=150",
+//   endTrigger: "html",
+//   end: "bottom bottom",
+//   pin: true,
+//   pinSpacing: false,
+// });
